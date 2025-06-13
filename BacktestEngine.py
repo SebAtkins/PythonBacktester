4b0252a07ee4960fcfdc2031eeca818b968c5eab
Seb <sebatkins@outlook.com>
2025-06-14 00:07:18 +0100

    Added first files

---
+++ BacktestEngine.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from tqdm import tqdm
+
+class Engine():
+    def __init__(self):
+        pass
+
+class Strategy():
+    def __init__(self):
+        pass
+
+class Trade():
+    def __init__(self):
+        pass
+
+class Order():
+    def __init__(self):
+        pass
+
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# PythonBacktester
+# Python Backtester
+This is a small project to try to create a simple backtesting engine in Python.
+
+## Installation
+- Clone repository
+- CD into repository
+- Run: `pip install -r requirements.txt`
+
